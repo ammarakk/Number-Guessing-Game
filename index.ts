cae1237fe999bbbfbfe70349c5bbfb12e3dc47c7
Ammar Ahmed Khan <161769688+ammarakk@users.noreply.github.com>
2024-03-23 01:29:32 +0500

    created gussing number game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+const randomnumber = Math.floor(Math.random() *6+1);
+console.log("Welcome to Number Guessing Game");
+
+
+const answers= await inquirer.prompt([
+    {
+  name: "userguessnumber",
+  type: "number",
+  message: "Please Guess a number between 1 - 6:",
+    }
+]);
+  if (answers.userguessnumber === randomnumber) {
+    console.log(" Congtatulation! you guessed right number.");
+  } else { 
+    console.log(" You guessed wrong number! Better luck next time ");
+  }
